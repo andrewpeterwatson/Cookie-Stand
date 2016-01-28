f8dac9b72aa0bfa3dc61ba1b68145fb8562c2a12
Andrew Watson <andrewpeterwatson@gmail.com>
2016-01-28 09:21:05 -0800

    merge conflict resolved

--- cookieStand/pikePlace.js
+++ cookieStand/pikePlace.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var hours = ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 var locations = ["Pike Place","SeaTac","Southcenter","Bellevue","Alki"];
 var storeId = ["pp", "st", "sc", "bl", "al"];
@@ -32,7 +31,6 @@ for (var i = 0; i < locations.length; i++) {
   newShops.push(new Shop(locations[i],minCust[i],maxCust[i],aveCook[i]));
 
   }
-
   Shop.prototype.render = function() {
     var headerH1 = document.createElement("h1")
     var sectionEl = document.getElementById("saleList");
@@ -40,6 +38,7 @@ for (var i = 0; i < locations.length; i++) {
     for (var i = 0; i < newShops.length; i++) {
       var liEl = document.createElement("li");
       liEl.textContent = this.stores;
+      headerH1.appendChild(liEl);
       var nestedUl = document.createElement("ul");
 
     for (var i = 0; i < this.cookiesPerHour.length; i++) {
@@ -47,9 +46,8 @@ for (var i = 0; i < locations.length; i++) {
       nestedLi.textContent = this.cookiesPerHour[i];
       nestedUl.appendChild(nestedLi);
     }
+    ulEl.appendChild(headerH1)
       ulEl.appendChild(nestedUl);
-      headerH1.appendChild(liEl);
-      ulEl.appendChild(headerH1)
     }
     sectionEl.appendChild(ulEl);
   }
@@ -57,162 +55,3 @@ for (var i = 0; i < locations.length; i++) {
     newShops[i].getCookies();
     newShops[i].render();
   }
-=======
-var shops = []
-var sectionEl = document.getElementById("saleListPike");
-var headerEl = document.createElement("h1")
-var ulEl = document.createElement("ul");
-var hours = ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"];
-var shop = {
-  location: "Pike Place",
-  minCustomer: 17,
-  maxCustomer: 88,
-  aveCookiesPerCustomer: 5.2,
-  cookiesPerHour: [],
-  totalSales: [],
-};
-shop.randomNum = function(min, max) {
-  return Math.random() * (max - min) + min;
-};
-for (var i = 0; i < hours.length; i++) {
-  randomCustomerNum = shop.randomNum(shop.minCustomer,shop.maxCustomer);
-  var cookiesSold = Math.floor(randomCustomerNum * shop.aveCookiesPerCustomer);
-  shop.cookiesPerHour.push(hours[i] + ": " + cookiesSold);
-};
-shop.totalSales.push(Math.floor(cookiesSold * randomCustomerNum));
-shop.cookiesPerHour.push("Total: " + shop.totalSales)
-headerEl.textContent = shop.location;
-sectionEl.appendChild(headerEl);
-for (var i = 0; i < shop.cookiesPerHour.length; i++) {
-  var liEl = document.createElement("li");
-  liEl.textContent = shop.cookiesPerHour[i];
-  ulEl.appendChild(liEl);
-  sectionEl.appendChild(ulEl);
-}
-
-shops.push(shops);
-
-var sectionEl = document.getElementById("saleListPike");
-var headerEl = document.createElement("h1")
-var ulEl = document.createElement("ul");
-var hours = ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"];
-var shop = {
-  location: "SeaTac",
-  minCustomer: 6,
-  maxCustomer: 24,
-  aveCookiesPerCustomer: 1.2,
-  cookiesPerHour: [],
-  totalSales: [],
-};
-shop.randomNum = function(min, max) {
-  return Math.random() * (max - min) + min;
-};
-for (var i = 0; i < hours.length; i++) {
-  randomCustomerNum = shop.randomNum(shop.minCustomer,shop.maxCustomer);
-  var cookiesSold = Math.floor(randomCustomerNum * shop.aveCookiesPerCustomer);
-  shop.cookiesPerHour.push(hours[i] + ": " + cookiesSold);
-};
-shop.totalSales.push(Math.floor(cookiesSold * randomCustomerNum));
-shop.cookiesPerHour.push("Total: " + shop.totalSales)
-headerEl.textContent = shop.location;
-sectionEl.appendChild(headerEl);
-for (var i = 0; i < shop.cookiesPerHour.length; i++) {
-  var liEl = document.createElement("li");
-  liEl.textContent = shop.cookiesPerHour[i];
-  ulEl.appendChild(liEl);
-  sectionEl.appendChild(ulEl);
-}
-
-var sectionEl = document.getElementById("saleListPike");
-var headerEl = document.createElement("h1")
-var ulEl = document.createElement("ul");
-var hours = ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"];
-var shop = {
-  location: "Southcenter",
-  minCustomer: 11,
-  maxCustomer: 38,
-  aveCookiesPerCustomer: 1.9,
-  cookiesPerHour: [],
-  totalSales: [],
-};
-shop.randomNum = function(min, max) {
-  return Math.random() * (max - min) + min;
-};
-for (var i = 0; i < hours.length; i++) {
-  randomCustomerNum = shop.randomNum(shop.minCustomer,shop.maxCustomer);
-  var cookiesSold = Math.floor(randomCustomerNum * shop.aveCookiesPerCustomer);
-  shop.cookiesPerHour.push(hours[i] + ": " + cookiesSold);
-};
-shop.totalSales.push(Math.floor(cookiesSold * randomCustomerNum));
-shop.cookiesPerHour.push("Total: " + shop.totalSales)
-headerEl.textContent = shop.location;
-sectionEl.appendChild(headerEl);
-for (var i = 0; i < shop.cookiesPerHour.length; i++) {
-  var liEl = document.createElement("li");
-  liEl.textContent = shop.cookiesPerHour[i];
-  ulEl.appendChild(liEl);
-  sectionEl.appendChild(ulEl);
-}
-
-var sectionEl = document.getElementById("saleListPike");
-var headerEl = document.createElement("h1")
-var ulEl = document.createElement("ul");
-var hours = ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"];
-var shop = {
-  location: "Bellevue",
-  minCustomer: 20,
-  maxCustomer: 48,
-  aveCookiesPerCustomer: 3.3,
-  cookiesPerHour: [],
-  totalSales: [],
-};
-shop.randomNum = function(min, max) {
-  return Math.random() * (max - min) + min;
-};
-for (var i = 0; i < hours.length; i++) {
-  randomCustomerNum = shop.randomNum(shop.minCustomer,shop.maxCustomer);
-  var cookiesSold = Math.floor(randomCustomerNum * shop.aveCookiesPerCustomer);
-  shop.cookiesPerHour.push(hours[i] + ": " + cookiesSold);
-};
-shop.totalSales.push(Math.floor(cookiesSold * randomCustomerNum));
-shop.cookiesPerHour.push("Total: " + shop.totalSales)
-headerEl.textContent = shop.location;
-sectionEl.appendChild(headerEl);
-for (var i = 0; i < shop.cookiesPerHour.length; i++) {
-  var liEl = document.createElement("li");
-  liEl.textContent = shop.cookiesPerHour[i];
-  ulEl.appendChild(liEl);
-  sectionEl.appendChild(ulEl);
-}
-
-var sectionEl = document.getElementById("saleListPike");
-var headerEl = document.createElement("h1")
-var ulEl = document.createElement("ul");
-var hours = ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"];
-var shop = {
-  location: "Alki",
-  minCustomer: 3,
-  maxCustomer: 24,
-  aveCookiesPerCustomer: 2.6,
-  cookiesPerHour: [],
-  totalSales: [],
-};
-shop.randomNum = function(min, max) {
-  return Math.random() * (max - min) + min;
-};
-for (var i = 0; i < hours.length; i++) {
-  randomCustomerNum = shop.randomNum(shop.minCustomer,shop.maxCustomer);
-  var cookiesSold = Math.floor(randomCustomerNum * shop.aveCookiesPerCustomer);
-  shop.cookiesPerHour.push(hours[i] + ": " + cookiesSold);
-};
-shop.totalSales.push(Math.floor(cookiesSold * randomCustomerNum));
-shop.cookiesPerHour.push("Total: " + shop.totalSales)
-headerEl.textContent = shop.location;
-sectionEl.appendChild(headerEl);
-for (var i = 0; i < shop.cookiesPerHour.length; i++) {
-  var liEl = document.createElement("li");
-  liEl.textContent = shop.cookiesPerHour[i];
-  ulEl.appendChild(liEl);
-  sectionEl.appendChild(ulEl);
-}
->>>>>>> ea0b4864618168ac89c68f05bd02fcbfdfb6aabe
